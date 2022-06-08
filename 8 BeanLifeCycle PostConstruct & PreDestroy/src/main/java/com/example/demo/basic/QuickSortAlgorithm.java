47cb16f8747dc34fe2aae6782ea4d532346b4178
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-06-08 20:02:05 +0200

    Add files via upload

---
+++ 8 BeanLifeCycle PostConstruct & PreDestroy/src/main/java/com/example/demo/basic/QuickSortAlgorithm.java
@@ -0,0 +1,11 @@
+package com.example.demo.basic;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuickSortAlgorithm implements SortAlgorithm{
+	public int[] sort(int[] numbers) {
+		//quick sort logic
+		return numbers;
+	}
+}
