d2d26d75d9f57fe9187f04422826b1cc9166b748
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-07-10 17:28:11 +0200

    Spring AOP

--- 14 FirstAPI/src/main/java/com/example/helloWorldAPI/HelloWorldApiApplication.java
+++ 14 FirstAPI/src/main/java/com/example/helloWorldAPI/HelloWorldApiApplication.java
@@ -1,13 +1,32 @@
 package com.example.helloWorldAPI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HelloWorldApiApplication {
+import com.example.helloWorldAPI.AOP.business_dao.Business1;
+import com.example.helloWorldAPI.AOP.business_dao.Business2;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class HelloWorldApiApplication implements CommandLineRunner{
+	@Autowired
+	Business1 business1;
+	@Autowired
+	Business2 business2;
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	public static void main(String[] args){
 		SpringApplication.run(HelloWorldApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info(business1.doSomething());
+		logger.info(business2.doSomething());
+	}
 }
