4b7082e73f20a36db0741d01fdd20d2dd4450295
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-07-15 16:04:54 +0200

    Add files via upload

--- 14 FirstAPI/src/main/java/com/example/helloWorldAPI/AOP/aspect/CommonJoinPointConfig.java
+++ 14 FirstAPI/src/main/java/com/example/helloWorldAPI/AOP/aspect/CommonJoinPointConfig.java
@@ -5,4 +5,16 @@ import org.aspectj.lang.annotation.Pointcut;
 public class CommonJoinPointConfig {
 	@Pointcut("execution(* com.example.helloWorldAPI.AOP.business_dao.*.*(..))")
 	public void businessLayerExecution(){}
+	
+	//all the method calls on beans containing "dao" in the name are matched by the Pointcut
+	@Pointcut("bean(*dao*)")
+	public void beanContainingDao(){}
+	
+	//all the method calls on beans starting with "dao" in the name are matched by the Pointcut
+	@Pointcut("bean(dao*)")
+	public void beanStartingDao(){}
+	
+	//all the method calls on beans ending on "dao" in the name are matched by the Pointcut
+	@Pointcut("bean(*dao)")
+	public void beanEndingDao(){}
 }
