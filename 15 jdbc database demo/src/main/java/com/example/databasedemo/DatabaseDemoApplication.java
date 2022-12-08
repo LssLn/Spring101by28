e3dae845e1b139ccbc7af5e89de663dd10a0d019
Alessio Leone <36887074+redbite@users.noreply.github.com>
2022-12-08 20:19:11 +0100

    Add files via upload

--- 15 jdbc database demo/src/main/java/com/example/databasedemo/DatabaseDemoApplication.java
+++ 15 jdbc database demo/src/main/java/com/example/databasedemo/DatabaseDemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.databasedemo;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.databasedemo.entity.Person;
 import com.example.databasedemo.jdbc.PersonJdbcDao;
 
 @SpringBootApplication
@@ -25,7 +28,15 @@ public class DatabaseDemoApplication implements CommandLineRunner{
 		logger.info("All users -> {}", dao.findAll());
 		int id = 10001;
 		logger.info("User with id {} -> {}",id, dao.findById(id));
+		
 		logger.info("Deleting user with id {}",id,dao.deleteById(id));
 		logger.info("All users -> {}", dao.findAll());
+		
+		logger.info("Inserting new User -> {}", dao.insert(new Person(10004,"Miguel","SP",new Date())));
+		
+		logger.info("Update 10003 -> {}", 
+				dao.update(new Person(10003,"Edmond","DE", new Date())));
+		logger.info("All users -> {}", dao.findAll());
+
 	}
 }
